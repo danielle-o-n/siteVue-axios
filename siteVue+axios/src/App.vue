a8b6dec8c8cb9be421ac0dc1fa6cfb427fda15fc
Danielle Oliveira <danielle.o.n@hotmail.com>
2023-01-27 13:06:46 -0300

    Add files via upload

---
+++ siteVue+axios/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    NavBar,
+  },
+};
+</script>
+
+<style>
+</style>
